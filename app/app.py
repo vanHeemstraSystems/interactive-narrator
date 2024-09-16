b6fca2e67480c31d08b9f2ff673751395ea615b7
Willem van Heemstra <wvanheemstra@icloud.com>
2024-09-16 14:45:47 +0000

    Update

--- app/app.py
+++ app/app.py
@@ -1,4 +1,14 @@
-from flask import Flask
+#!/usr/bin/env python
+import sys
+import os
+import math
+import json
+import datetime, time
+
+# tell python where to look for packages
+# sys.path.append('/var/www/interactivenarrator')
+
+from flask import Flask, jsonify, render_template, url_for, request
 from markupsafe import escape
 
 app = Flask(__name__)
