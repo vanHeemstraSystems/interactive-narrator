2f8a538d2721ccedc609a85019133a9fca39cb12
Willem van Heemstra <wvanheemstra@icloud.com>
2024-09-18 10:39:10 +0000

    Update

--- app/app.py
+++ app/app.py
@@ -11,6 +11,7 @@ sys.path.append('/var/www/interactivenarrator')
 from sqlalchemy import create_engine, select, update, func
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import and_, not_, or_
+from slqalchemy.sql import func
 from flask import Flask, jsonify, render_template, url_for, request
 from flask import flash, send_from_directory, Response, session
 from flask_sqlalchemy import SQLAlchemy
@@ -36,8 +37,27 @@ sys.path.append('/var/www/VisualNarrator')
 app = Flask(__name__)
 app.config.from_object(config)
 
-# db = SQLAlchemy(app)
+# define a database
+db = SQLAlchemy(app)
 # db.Model = Base
+
+# MORE
+
+# Models: TO DO - In future move these to models.py
+
+class Student(db.Model):
+    __tablename__ = 'student'
+    id = db.Column(db.Integer(), primary_key=True)
+    firstname = db.Column(db.String(100), nullable=False)
+    lastname = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    age = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime(timezone=True),
+                        server_default=func.now())
+    bio = db.Column(db.Text)
+
+    def __repr__(self):
+        return f'<Student {self.firstname}>'
 
 # MORE
 
