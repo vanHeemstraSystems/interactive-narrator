b1cb9ecf0dfde9a0813093f9e0de2d32d674cc6a
Willem van Heemstra <wvanheemstra@icloud.com>
2024-09-25 13:31:44 +0000

    Fix username

--- flask_app/app/form.py
+++ flask_app/app/form.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 from wtforms import Form, BooleanField
+from wtforms import StringField, SubmitField, IntegerField, PasswordField, TextAreaField
+from wtforms import validators, ValidationError
+from wtforms.validators import Length, DataRequired, Email
 
 class RegistrationForm(Form):
     username = StringField('Username', [validators.Length(min=4, max=20)])
--- flask_app/app/register/routes.py
+++ flask_app/app/register/routes.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python
-from flask import render_template
+from flask import render_template, request
 from app.register import bp
 
 from app.form import RegistrationForm
 
 @bp.route('/register', methods=['GET', 'POST'])
 def do_register():
-
+  try:
     form = RegistrationForm(request.form)
+    if request.method == "POST" and form.validate():
+        print('validating was a success')
 
-    username = form.username.data
+        username = form.username.data
+
+    else:
+      return render_template("register.html", form=form)
+
+  except Exception as e:
+    print('an exception occured', e)
 
     error = 'Sorry, we could not register you.'
 
