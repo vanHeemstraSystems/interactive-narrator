26a8f70d595de967f48a61d168e734b746ffee8e
Willem van Heemstra <wvanheemstra@icloud.com>
2024-09-25 14:12:26 +0000

    Update

---
+++ flask_app/app/post.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+
+
+def add_data_to_db(us_instances, output_ontobj, output_prologobj, m, form_data):
+    # get the starting position for the new userstory IDs, based on the presence/absence of
+    # user stories already in the database
+
+    print('Add Data To DB')
--- flask_app/app/register/routes.py
+++ flask_app/app/register/routes.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from flask import render_template, request
+from passlib.hash import sha256_crypt
 from app.register import bp
 
 from app.form import RegistrationForm
@@ -12,6 +13,9 @@ def do_register():
         print('validating was a success')
         username = form.username.data
         email = form.email.data
+        password = sha256_crypt.encrypt((str(form.password.data)))
+
+        print('success, user does not exist yet')
 
     else:
       return render_template("register.html", form=form)
