a5929a2659670da80a119fd929ef5b19eca490c2
Willem van Heemstra <wvanheemstra@icloud.com>
2024-09-26 13:15:41 +0000

    Update

--- flask_app/app/models/user.py
+++ flask_app/app/models/user.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python
 from app.extensions import db
+from datetime import datetime
+# from sqlalchemy import Column, Table, DateTime, Boolean, String
 
 class User(db.Model):
+    """A user login, with credentials and authentication."""
+    __tablename__ = 'user'
+
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150))
-    email = db.Column(db.String(150))
-    password = db.Column(db.String(150))
+    created = db.Column(db.DateTime, default=datetime.now)
+    modified = db.Column(db.DateTime, default=datetime.now,
+                      onupdate=datetime.now)    
+    username = db.Column('username', db.String(200))
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    active = db.Column(db.Boolean, default=True)
+    password = db.Column('password', db.String(100))
+    last_login_at = db.Column(db.DateTime())
+    current_login_at = db.Column(db.DateTime())
+    last_login_ip = db.Column(db.String(100))
+    current_login_ip = db.Column(db.String(100))
+    login_count = db.Column(db.Integer)
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
 
     def __repr__(self):
         return f'<User "{self.username}">'
--- flask_app/app/post.py
+++ flask_app/app/post.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
-
-
+from app import sqlsession
+from app.user_stories import UserStoryVN
 
 def add_data_to_db(us_instances, output_ontobj, output_prologobj, m, form_data):
     # get the starting position for the new userstory IDs, based on the presence/absence of
     # user stories already in the database
-
-    print('Add Data To DB')
+    starting_id = sqlsession.query(UserStoryVN).order_by(UserStoryVN.id.desc()).first()
+    if starting_id is None:
+        starting_id = 0
+    else:
+        starting_id = starting_id.id
+    print('FIRST STARTING_ID', starting_id)
